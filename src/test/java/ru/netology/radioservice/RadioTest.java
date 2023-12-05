796fc155dae9bbb6d581107df65f5f9c47088226
Anna Shcherbakova <ShcherbakovaAnna90@yandex.ru>
2023-12-05 21:46:49 +0600

    New test

--- src/test/java/ru/netology/radioservice/RadioTest.java
+++ src/test/java/ru/netology/radioservice/RadioTest.java
@@ -5,6 +5,22 @@ import org.junit.jupiter.api.Test;
 
 public class RadioTest {
     Radio radio = new Radio();
+    @Test
+    public void shouldSetDefaultChanelAmount() {
+        int expected = 10;
+        int actual = radio.getChanelAmount();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSetDefiniteChanelAmount() {
+        Radio radio = new Radio(50);
+        int expected = 50;
+        int actual = radio.getChanelAmount();
+
+        Assertions.assertEquals(expected, actual);
+    }
 
     @Test
     public void shouldSetChanel() {
